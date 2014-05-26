8338de859880458d503d182a5a5d2a70ad61a598
mingang.he <dustgle@gmail.com>
2014-05-26 13:51:25 +0800

    del debuglog

--- debug_print.h
+++ debug_print.h
@@ -3,46 +3,14 @@
 
 #include <stdio.h>
 #include <string.h>
-
-#define debug_print(fmt, ...) \
-    do { \
-        if (DEBUG_PRINT) \
-            fprintf(stderr, "debug_print: %s: %d: %s():" \
-                    fmt "\n", __FILE__, __LINE__, __func__, \
-                    ##__VA_ARGS__); \
-    } while (0)
-
-#if DEBUG_LOG
-FILE *DEBUG_LOG_FILE;
-char DEBUG_STRING[1024];
-#else
-FILE *DEBUG_LOG_FILE;
-char *DEBUG_STRING;
-# ifndef NDEBUG
-#   define NDEBUG
-# endif
-#endif
-
 #include <assert.h>
 
-#define open_debug_log(filename) do { \
-    if (DEBUG_LOG) { \
-        DEBUG_LOG_FILE = fopen(filename, "w"); \
-    } \
-} while (0)
-
-#define debug_log(fmt, ...) do { \
-    if (DEBUG_LOG) { \
-        snprintf(DEBUG_STRING, 1023, "%s: %s(): %d: " fmt "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__); \
-        fwrite(DEBUG_STRING, 1, strlen(DEBUG_STRING), DEBUG_LOG_FILE); \
-        fflush(DEBUG_LOG_FILE); \
-    } \
-} while (0)
-
-#define close_debug_log() do { \
-    if (DEBUG_LOG) { \
-        fclose(DEBUG_LOG_FILE); \
-    } \
-} while (0)
+#define debug_print(fmt, ...) \
+	do { \
+		if (DEBUG_PRINT) \
+		fprintf(stderr, "debug_print: %s: %d: %s():" \
+			fmt "\n", __FILE__, __LINE__, __func__, \
+			##__VA_ARGS__); \
+	} while (0)
 
 #endif
